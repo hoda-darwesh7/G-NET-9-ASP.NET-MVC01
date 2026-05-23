ead98e6178572ad29a348517656a779bb83d64f1
Mahmoud Abdel Sami <Mahmoud.Abdel.Sami113@science.helwan.edu.eg>
2026-05-24 00:11:13 +0300

    Demo

---
+++ Context/GymDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_1.Configrations;
+using MVC_1.Models;
+using System.Numerics;
+
+namespace MVC_1.Context
+{
+    public class GymDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = 7ODA ; Database = GymDb ; Trusted_Connection = true ; TrustServerCertificate = true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Plan>(new PlanConfigration());
+        }
+        public DbSet<Plan> Plans { get; set; }
+    }
+}
---
+++ Controllers/PlanController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_1.Context;
+using MVC_1.Migrations;
+
+namespace MVC_1.Controllers
+{
+    public class PlanController : Controller
+    {
+        private readonly GymDbContext  context;
+
+        public PlanController()
+        {
+            context = new GymDbContext();
+        }
+
+        // GET ::BaseUrl/Plan/Index
+        public async Task<IActionResult> Index()
+        {
+            var plans = await context.Plans.ToListAsync();
+            return View(plans);
+        }
+
+        // GET ::BaseUrl/Plan/Details
+        public async Task<IActionResult> Details(int id)
+        {
+            var plan = await context.Plans.FindAsync(id);
+            if (plan == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+
+    }
+}
